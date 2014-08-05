ccc7b5a3b3c6446aad81478a47ebf7f7935b9870
junhanamaki <jun.hanamaki@gmail.com>
2014-08-05 19:39:43 +0100

    commit to sync

---
+++ lib/active_model_validators_ex.rb
@@ -0,0 +1,5 @@
+require "active_model_validators_ex/version"
+
+module ActiveModelValidatorsEx
+  # Your code goes here...
+end
---
+++ lib/active_model_validators_ex/version.rb
@@ -0,0 +1,3 @@
+module ActiveModelValidatorsEx
+  VERSION = "0.0.1"
+end

fdfe7b1b6c2e7b336ba49ff9d352912b3790934f
junhanamaki <jun.hanamaki@gmail.com>
2014-08-07 15:43:00 +0100

    range test

--- spec/active_model_validators_ex/array_inclusion_validator_spec.rb
+++ spec/active_model_validators_ex/array_inclusion_validator_spec.rb
@@ -19,11 +19,33 @@ describe ArrayInclusionValidator do
     let(:validator) { ArrayInclusionValidator.new(options) }
     before { validator.validate_each(record, attribute, value) }
 
-    context 'for instance initialized with options hash ' \
-            'and in with an Array of values' do
+    context 'for instance initialized with key in with an Array of values' do
       let(:in_array) { [1, 2, 3] }
       let(:options) do
         { attributes: attribute, in: in_array }
+      end
+
+      context 'when passed value is an array with values that are in options array' do
+        let(:value) { [1, 2, 3] }
+
+        it 'does not set error messages in record' do
+          expect(record.errors[attribute].count).to eq(0)
+        end
+      end
+
+      context 'when passed value is an array with values not in options array' do
+        let(:value) { [4, 5, 6] }
+
+        it 'sets error message in record' do
+          expect(record.errors[attribute].count).to eq(1)
+        end
+      end
+    end
+
+    context 'for instance initialized with key in with a Range of values' do
+      let(:in_range) { 1..3 }
+      let(:options) do
+        { attributes: attribute, in: in_range }
       end
 
       context 'when passed value is an array with values that are in options array' do
